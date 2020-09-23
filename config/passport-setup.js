7132485d2ac7ce8dd6803f99e28cfb976bcf805d
Akshay Shahi <shahiakshay31@gmail.com>
2020-09-23 21:23:07 +0530

    added github strategy and client secret configs

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,8 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GithubStrategy = require('passport-github').Strategy;
 
-const { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, GOOGLE_CLIENTID, GOOGLE_CLIENT_SECRET } = process.env;
+const { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, GOOGLE_CLIENTID, GOOGLE_CLIENT_SECRET, GITHUB_CLIENT_SECRET, GITHUB_CLIENT_ID } = process.env;
 const UserModel = require('../db/models/UserModel');
 
 passport.serializeUser((user, done) => {
@@ -63,6 +64,37 @@ passport.use(
                     appId: profile.id,
                     username: profile.displayName,
                     thumbnail: profile._json.picture,
+                    provider: profile.provider
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+)
+
+passport.use(
+    new GithubStrategy({
+        //options for google strategy
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: 'http://localhost:3061/auth/github/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        //passport callback function
+        console.log('github profile =?', profile);
+        UserModel.findOne({appId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new UserModel({
+                    appId: profile.id,
+                    displayName: profile.displayName,
+                    username: profile.username,
+                    thumbnail: profile._json.avatar_url,
                     provider: profile.provider
                 }).save().then((newUser) => {
                     console.log('created new user: ', newUser);
